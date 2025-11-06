94f8155f353633953418ef650d98145d9652d393
Daniel Sabitov <danil.snip@gmail.com>
2025-11-07 03:21:20 +0500

    'start-template;

---
+++ nuxt.config.ts
@@ -0,0 +1,100 @@
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    
+    ssr: true,
+
+    server: {
+        port: process.env.NITRO_PORT || 8000,
+        host: process.env.NITRO_HOST || '127.0.0.1'
+    },
+
+    app: {
+        head: {
+            title: 'Avione — дешёвые авиабилеты онлайн',
+            htmlAttrs: {
+                lang: 'ru'
+            },
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                {
+                    name: 'description',
+                    content: 'Avione — онлайн продажа авиабилетов в Ташкенте. Покупай дешёвые авиабилеты быстро и удобно.'
+                },
+                {
+                    name: 'keywords',
+                    content: 'авиабилеты, купить билет, дешёвые авиабилеты, Ташкент, Avione, онлайн бронирование'
+                }
+            ],
+        },
+        pageTransition: false
+    },
+
+    experimental: {
+        clientRouteNavigation: false,
+        payloadExtraction: false
+    },
+
+    runtimeConfig: {
+        public: {
+            siteBaseURL: process.env.SITE_BASE_URL,
+            apiBaseURL: process.env.API_BASE_URL
+        }
+    },
+    
+
+    devtools: { enabled: true },
+    modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+ 
+    css: [
+        '@/assets/styles/main.scss'
+    ],
+
+    vite: {
+        build: {
+            target: 'esnext',
+            cssCodeSplit: false,
+            sourcemap: false,
+            minify: 'terser',
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                    pure_funcs: ['console.info', 'console.debug', 'console.warn'],
+                },
+                format: {
+                    comments: false,
+                },
+            },
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'vendor-core': ['vue', 'vue-router', 'pinia'],
+                        //'vendor-sentry': ['@sentry/vue', '@sentry/browser'],
+                        'vendor-swiper': ['swiper'],
+                        'vendor-gsap': ['gsap'],
+                    },
+                },
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "@/assets/styles/global/wrapper.scss";`
+                },
+            },
+        },
+        optimizeDeps: {
+            include: ['axios', 'pinia'],
+        },
+    },
+
+    alias: {
+        "@app": "/app",
+        "@entities": "/entities",
+        "@features": "/features",
+        "@shared": "/shared",
+        "@widgets": "/widgets",
+        "@api": "/api",
+    },
+})
